376bb85312c0ce40c5b50ff4a30c4a287948ef7b
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2026-07-29 19:20:43 +0530

    Update chatsdb.py

--- Chizuru/core/mongo/chatsdb.py
+++ Chizuru/core/mongo/chatsdb.py
@@ -3,21 +3,25 @@ from config import Config
 
 class ChatsDB:
     def __init__(self):
-        self.client = motor.motor_asyncio.AsyncIOMotorClient(Config.MONGO_URL)
-        self.db = self.client['ChizuruMusic']
-        self.collection = self.db['chats']
+        if Config.MONGO_URL:
+            self.client = motor.motor_asyncio.AsyncIOMotorClient(Config.MONGO_URL)
+            self.db = self.client['ChizuruMusic']
+            self.collection = self.db['chats']
     
     async def add_chat(self, chat_id):
+        if not Config.MONGO_URL:
+            return
         if not await self.get_chat(chat_id):
             await self.collection.insert_one({'chat_id': chat_id})
     
     async def get_chat(self, chat_id):
+        if not Config.MONGO_URL:
+            return None
         return await self.collection.find_one({'chat_id': chat_id})
     
     async def remove_chat(self, chat_id):
+        if not Config.MONGO_URL:
+            return
         await self.collection.delete_one({'chat_id': chat_id})
-    
-    async def get_all_chats(self):
-        return await self.collection.find().to_list(None)
 
 chats_db = ChatsDB()
