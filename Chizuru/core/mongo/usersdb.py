24775a1e6d6a39cda30e1142c64664750640965e
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2026-07-29 19:21:36 +0530

    Update usersdb.py

--- Chizuru/core/mongo/usersdb.py
+++ Chizuru/core/mongo/usersdb.py
@@ -3,21 +3,20 @@ from config import Config
 
 class UsersDB:
     def __init__(self):
-        self.client = motor.motor_asyncio.AsyncIOMotorClient(Config.MONGO_URL)
-        self.db = self.client['ChizuruMusic']
-        self.collection = self.db['users']
+        if Config.MONGO_URL:
+            self.client = motor.motor_asyncio.AsyncIOMotorClient(Config.MONGO_URL)
+            self.db = self.client['ChizuruMusic']
+            self.collection = self.db['users']
     
     async def add_user(self, user_id, username=""):
+        if not Config.MONGO_URL:
+            return
         if not await self.get_user(user_id):
-            await self.collection.insert_one({
-                'user_id': user_id,
-                'username': username
-            })
+            await self.collection.insert_one({'user_id': user_id, 'username': username})
     
     async def get_user(self, user_id):
+        if not Config.MONGO_URL:
+            return None
         return await self.collection.find_one({'user_id': user_id})
-    
-    async def get_all_users(self):
-        return await self.collection.find().to_list(None)
 
 users_db = UsersDB()
