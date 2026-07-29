946de4e8a53bbfa9be99e39177999974bbe42735
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2026-07-29 17:00:53 +0530

    Create admin_func.py

---
+++ Chizuru/core/admin_func.py
@@ -0,0 +1,24 @@
+from pyrogram import filters
+from pyrogram.types import Message
+from functools import wraps
+from Chizuru import Chizuru
+from config import Config
+
+AUTHORIZED_USERS = [Config.OWNER_ID]
+
+def authorized_users(func):
+    @wraps(func)
+    async def wrapper(client, message: Message):
+        if message.from_user.id in AUTHORIZED_USERS:
+            return await func(client, message)
+        else:
+            await message.reply_text("❌ You are not authorized!")
+    return wrapper
+
+admins = {}
+
+async def set_admins(chat_id, admin_list):
+    admins[chat_id] = admin_list
+
+async def get_admins(chat_id):
+    return admins.get(chat_id, [])
